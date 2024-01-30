5eb8908d3fb11ae1130f69c10d366e01cf449384
harublank <harublank00@gmail.com>
2024-01-30 19:21:28 +0545

    add and create dialog modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <h3>hello admin dashboard</h3>;
+import { Modal } from "@/components/ui/modal";
+import { UserButton } from "@clerk/nextjs";
+
+export default function SetupPage() {
+  return (
+    <div className="p-4">
+      <Modal
+        isOpen
+        onClose={() => {}}
+        title="Test"
+        description="test descripton"
+      >
+        children
+      </Modal>
+    </div>
+  );
 }
