6b7d35dcc0db78b57a3191baaa049290c117b812
harublank <harublank00@gmail.com>
2024-02-10 20:26:28 +0545

    show color for color value

--- app/(dashboard)/[storeId]/(routes)/colors/[colorId]/color-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/[colorId]/color-form.tsx
@@ -29,7 +29,7 @@ interface ColorFormProps {
 
 const formSchema = z.object({
   name: z.string().min(1, { message: "Please enter size name" }),
-  value: z.string().min(1, { message: "Please enter size value" }),
+  value: z.string().min(4).regex(/^#/, { message: "Please enter size value" }),
 });
 
 type SizeFormValues = z.infer<typeof formSchema>;
@@ -152,11 +152,17 @@ const SizeForm: React.FC<ColorFormProps> = ({ initialData }) => {
                 <FormItem>
                   <FormLabel>Value</FormLabel>
                   <FormControl>
-                    <Input
-                      disabled={loading}
-                      placeholder="Color value"
-                      {...field}
-                    />
+                    <div className="flex items-center gap-x-4">
+                      <Input
+                        disabled={loading}
+                        placeholder="Color value"
+                        {...field}
+                      />
+                      <div
+                        className="border p-4 rounded-full"
+                        style={{ backgroundColor: field.value }}
+                      ></div>
+                    </div>
                   </FormControl>
                   <FormMessage />
                 </FormItem>
--- app/(dashboard)/[storeId]/(routes)/colors/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/components/columns.tsx
@@ -8,6 +8,7 @@ import { CellAction } from "./cell-actions";
 export type ColorsColumn = {
   id: string;
   name: string;
+  value: string;
   createdAt: string;
 };
 
@@ -26,6 +27,18 @@ export const columns: ColumnDef<ColorsColumn>[] = [
   {
     accessorKey: "value",
     header: "Value",
+    cell: ({ row }) => (
+      <div className="flex items-center gap-x-2">
+        {row.original.value}
+
+        <div
+          className="h-6 w-6 rounded-full border"
+          style={{
+            backgroundColor: row.original.value,
+          }}
+        ></div>
+      </div>
+    ),
   },
   {
     accessorKey: "createdAt",
